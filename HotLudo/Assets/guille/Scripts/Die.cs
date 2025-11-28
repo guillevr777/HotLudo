3f5c00609f4a8203fa473a816c62a63cf12920c1
guillevr777 <gs.villanueva@iesnervion.es>
2025-11-28 10:58:42 +0100

    a

---
+++ HotLudo/Assets/guille/Scripts/Die.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    [SerializeField]
+    List<Sprite> die;
+
+    int roll;
+
+    public void RollRandom()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = die[Random.Range(0, die.Count)];
+    }
+
+    public void RollDie(int temp)
+    {
+        roll = temp;
+        Animator animator = GetComponent<Animator>();
+        animator.Play("Roll", -1, 0f);
+    }
+
+    public void SetRoll()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = die[roll - 1];
+        GameManager.instance.RollEnd();
+    }
+}
---
+++ HotLudo/Assets/guille/Scripts/Turn.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    [SerializeField]
+    Player _player;
+
+    [SerializeField]
+    GameObject playerImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.Message += SetTurn;
+    }
+
+    void SetTurn(Player player)
+    {
+        playerImage.SetActive(player == _player);
+    }
+}
