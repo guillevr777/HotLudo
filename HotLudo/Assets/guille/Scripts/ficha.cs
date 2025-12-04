1a7c722843b98464e61b9fc06b39c2368c2b2d55
guillevr777 <gs.villanueva@iesnervion.es>
2025-12-04 21:46:46 +0100

    a

--- HotLudo/Assets/guille/Scripts/ficha.cs
+++ HotLudo/Assets/guille/Scripts/ficha.cs
@@ -5,6 +5,15 @@ public class Ficha : MonoBehaviour
     public ColorJugador color;
     public Casilla casillaActual;
 
+    private void Start()
+    {
+        if (casillaActual != null)
+        {
+            casillaActual.AñadirFicha(this);
+        }
+    }
+
+
     public void MoverACasilla(Casilla nuevaCasilla)
     {
         if (casillaActual != null)
